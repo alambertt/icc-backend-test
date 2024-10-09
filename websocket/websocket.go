f899bbe99ae02f536796730e476b9f693425f7a6
alambertt <github.vtddi@zico.slmail.me>
2024-10-09 00:48:03 -0400

    Refactor SendURLToPlayer function and remove unused code

--- websocket/websocket.go
+++ websocket/websocket.go
@@ -1,19 +1,18 @@
 package websocket
 
 import (
-	"fmt"
 	"icc-backend-test/model"
 )
 
 // SendURLToPlayers sends the URL of the game to the players using WebSockets. When the players receive the URL, they can start playing the game. The frontend will use the URL to redirect the players to the game.
 func SendURLToPlayer(player *model.Player, url string) {
-	message := map[string]string{
-		"type": "game_url",
-		"url":  url,
-	}
+	// message := map[string]string{
+	// 	"type": "game_url",
+	// 	"url":  url,
+	// }
 
 	// Send URL to white player
-	if err := player.Conn.WriteJSON(message); err != nil {
-		fmt.Println("Error sending URL to player 1:", err)
-	}
+	// if err := player.Conn.WriteJSON(message); err != nil {
+	// 	fmt.Println("Error sending URL to player 1:", err)
+	// }
 }
